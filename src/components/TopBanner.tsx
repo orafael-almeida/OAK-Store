31ae57581e61c5538b6fefddbe6e921f5a9b48a0
Rafael Almeida <almeidarp.dev@gmail.com>
2025-01-04 17:30:55 -0300

    feat: add links in top-banner elements and add oak-logo image

--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -4,17 +4,19 @@ import { FaLinkedinIn } from "react-icons/fa6";
 
 const TopBanner = () => {
   return (
-    <div className="flex flex-col justify-center items-center md:flex-row md:justify-between md:font-normal text-sm bg-primary text-white font-bold text-center p-3 font-sm tracking-tighter">
-      <div className="flex ml-4 gap-4">
+    <div className="flex flex-col justify-center items-center md:flex-row md:justify-between md:font-normal text-sm bg-primary text-white font-bold text-center p-3 font-sm lg2:tracking-tighter gap-4">
+      <div className="hidden lg2:flex ml-4 gap-4">
         <span>Central Comercial</span>
-        <span>(017) 98190-5881</span>
-        <span>(011) 3136-1594</span>
+        <Link href={"tel:55017981905881"}>(017) 98190-5881</Link>
+        <Link href={"tel:5501131361594"}>(011) 3136-1594</Link>
       </div>
       <p className="px-4 font-bold">
         Promovemos uma transformação digital efetiva em sua empresa
       </p>
-      <p className="font-bold">Vamos Conversar?</p>
-      <div className="flex gap-5 md:mr-14">
+      <Link href={"#contato"} className="hidden lg2:flex font-bold">
+        Vamos Conversar?
+      </Link>
+      <div className="flex gap-4 md:mr-14">
         <Link href={"https://www.linkedin.com/company/76051272/admin/"}>
           <FaLinkedinIn />
         </Link>
