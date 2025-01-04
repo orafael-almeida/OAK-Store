d607a8902aa44bf47b9f7f7a414313833da22714
Rafael Almeida <almeidarp.dev@gmail.com>
2025-01-04 18:51:51 -0300

    feat: add background dot image in main section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import dots from "../../public/assets/images/dot-back.png";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="px-5 lg2:px-10 ">
+      <main className="relative bg-primary rounded-2xl h-max text-white py-24 px-36">
+        <div
+          className="absolute inset-0 opacity-20"
+          style={{ backgroundImage: `url(${dots.src})` }}
+        />
+        <div className="text-5xl">teste</div>
+      </main>
+    </section>
+  );
 }
