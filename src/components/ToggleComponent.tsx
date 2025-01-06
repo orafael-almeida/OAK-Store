c8299fdf9afa83c9c54122773213102b528e1eb8
Rafael Almeida <almeidarp.dev@gmail.com>
2025-01-06 18:57:52 -0300

    fix: adjust toggle component to receive props

--- src/components/ToggleComponent.tsx
+++ src/components/ToggleComponent.tsx
@@ -1,11 +1,18 @@
 interface ToggleComponentProps {
   text: string;
+  checked: boolean;
+  onChange: (checked: boolean) => void;
 }
 
-const ToggleComponent = ({ text }: ToggleComponentProps) => {
+const ToggleComponent = ({ text, checked, onChange }: ToggleComponentProps) => {
   return (
     <label className="inline-flex items-center mb-5 cursor-pointer">
-      <input type="checkbox" value="" className="sr-only peer" />
+      <input
+        type="checkbox"
+        className="sr-only peer"
+        checked={checked}
+        onChange={(e) => onChange(e.target.checked)}
+      />
       <div className="relative w-11 h-6 peer-focus:outline-none rounded-full peer bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all peer-checked:bg-emerald-600"></div>
       <span className="ms-3 text-sm font-medium text-white">{text}</span>
     </label>
